5a3503752b85b0a5f696aae7d3612d61721bc54d
Yetzabeth Hernandez <6785@hbtn.uy>
2024-02-07 23:43:14 -0300

    obtaining location with google maps

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'firebase_options.dart';
 
@@ -14,29 +15,30 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    const myHomePage = MyHomePage();
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: myHomePage,
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late GoogleMapController mapController;
   String _currentAddress = 'Ubicación desconocida';
+  LatLng _initialCameraPosition = const LatLng(0, 0);
+  bool _isLocationLoaded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -44,44 +46,67 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Material App Bar'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                var status = await Permission.location.request(); // Solicitar permiso de ubicación
-                if (status.isGranted) {
-                  // Si se otorga el permiso de ubicación, obtener la ubicación actual
-                  Position position = await Geolocator.getCurrentPosition(
-                    desiredAccuracy: LocationAccuracy.high,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              var status = await Permission.location.request();
+              if (status.isGranted) {
+                Position position = await Geolocator.getCurrentPosition(
+                  desiredAccuracy: LocationAccuracy.high,
+                );
+                String address = await _getCurrentAddress(position);
+                setState(() {
+                  _currentAddress = address;
+                  _initialCameraPosition = LatLng(
+                    position.latitude,
+                    position.longitude,
                   );
-                  // ignore: use_build_context_synchronously
-                  String address = await _getCurrentAddress(position, context); // Pasar el contexto aquí
-                  setState(() {
-                    _currentAddress = address;
-                  });
-                } else {
-                  // Si no se otorga el permiso, mostrar un mensaje de error
-                  // ignore: use_build_context_synchronously
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Se requiere permiso de ubicación.'),
+                  _isLocationLoaded = true;
+                });
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Se requiere permiso de ubicación.'),
+                  ),
+                );
+              }
+            },
+            child: const Text('Actualizar ubicación'),
+          ),
+          const SizedBox(height: 20),
+          Text('Ubicación actual: $_currentAddress'),
+          const SizedBox(height: 20),
+          Expanded(
+            child: _isLocationLoaded
+                ? SizedBox(
+                    width: double.infinity,
+                    height: 300,
+                    child: GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                        target: _initialCameraPosition,
+                        zoom: 16,
+                      ),
+                      onMapCreated: (controller) {
+                        mapController = controller;
+                      },
+                      markers: {
+                        Marker(
+                          markerId: const MarkerId('userLocation'),
+                          position: _initialCameraPosition,
+                        ),
+                      },
                     ),
-                  );
-                }
-              },
-              child: const Text('Actualizar ubicación'),
-            ),
-            const SizedBox(height: 20),
-            Text('Ubicación actual: $_currentAddress'),
-          ],
-        ),
+                  )
+                : Center(child: CircularProgressIndicator()),
+          ),
+        ],
       ),
     );
   }
 
-  Future<String> _getCurrentAddress(Position position, BuildContext context) async {
+  Future<String> _getCurrentAddress(Position position) async {
     try {
       List<Placemark> placemarks = await placemarkFromCoordinates(
         position.latitude,
